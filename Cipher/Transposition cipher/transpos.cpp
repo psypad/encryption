fd1c2903dae519d9da48c52a763516e84711e30e
SylanPadmakumar <112978372+SylanPadmakumar@users.noreply.github.com>
2025-02-05 16:52:28 +0530

    string inserted into vector without spaces

--- Cipher/Transposition cipher/transpos.cpp
+++ Cipher/Transposition cipher/transpos.cpp
@@ -4,47 +4,33 @@
 
 using namespace std;
 
-void removeSpaces(char *str)
-{
-    // To keep track of non-space character count
-    int count = 0;
 
-    // Traverse the given string. If current character
-    // is not space, then place it at index 'count++'
-    for (int i = 0; str[i]; i++)
-        if (str[i] != ' ')
-            str[count++] = str[i]; // here count is
-                                   // incremented
-    str[count] = '\0';
-}
 
 int main(){
 
     cout << "enter the string to be encrypted" << endl;
 
-    char* plaintext;
+    string plaintext;
     
-    cin >> plaintext;
+    getline(cin, plaintext);
 
-    cout << plaintext << endl; 
-    
-    removeSpaces(plaintext);
-
-    cout << plaintext << endl;  
+    //cout << plaintext << endl; 
     
     vector<char> str_store;    
 
-    /*
+    int str_len = plaintext.length();
 
     for(int i=0; i<str_len;  i++){
-        str_store.insert(str_store.begin() + i, plaintext[i]);
+        if(plaintext[i] == ' '){
+            continue;
+        }else{
+            str_store.push_back(plaintext[i]);
+        }
     }
 
     for (int i = 0; i < str_store.size(); i++) {
         cout << str_store[i] << " ";
     }
-
-    */
 
     return 0;
 }
